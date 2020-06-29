b271d331e78d5717eee685b09c6cf709c6a6a0a3
cadz15 <m4rc24@yahoo.com>
2020-06-29 15:04:13 +0800

    Page Title @section("title", "Title of the page")

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'Magma | Login')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/register_user/step1.blade.php
+++ resources/views/register_user/step1.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'Register | Step 1')
 @section('content')
   <div class="container">
  
